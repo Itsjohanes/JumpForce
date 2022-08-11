1bf63a92ec88524586814fb632bdc1aff4fd90be
Itsjohanes <johannesap@upi.edu>
2022-08-11 20:01:42 +0700

    commit ketiga

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -6,25 +6,32 @@ public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
     private Rigidbody playerRb;
+    private AudioSource playerAudio;
+    public ParticleSystem dirtParticle;
     public bool gameOver;
+    public ParticleSystem explosionParticle;
     public float jumpForce = 10;
     public bool isOnGround = true;
     public float gravityModifier;
+    public AudioClip crashSound;
+    public AudioClip jumpSound;
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
         // playerRb.AddForce(Vector3.up * 1000);
         Physics.gravity *= gravityModifier;
+        playerAudio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
-
+            dirtParticle.Stop();
+            playerAudio.PlayOneShot(jumpSound, 1.0f);
 
         }
 
@@ -35,12 +42,17 @@ public class PlayerController : MonoBehaviour
         if (collision.gameObject.CompareTag("Ground"))
         {
             isOnGround = true;
+            dirtParticle.Play();
 
         }
         else if(collision.gameObject.CompareTag("Obstacle"))
         {
+            
             gameOver = true;
             Debug.Log("Game Over");
+            explosionParticle.Play();
+            dirtParticle.Stop();
+            playerAudio.PlayOneShot(crashSound, 1.0f);
         }
     }
 }
